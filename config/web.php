a0c2bf387743df52dfe7b0680701d3b918f4c238
vi <Vi@p-team.ru>
2025-05-05 15:50:01 +0300

    start

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'KkNDfLoyrYLTWmhHCZk8vas1y2VO4n-7',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'user'],
             ],
-        ],
-        */
+        ]
     ],
     'params' => $params,
 ];
